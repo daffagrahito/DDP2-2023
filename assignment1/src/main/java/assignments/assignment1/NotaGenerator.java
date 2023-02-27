993517a04c08556b8de7933d1c8b430551b510ea
daffagrahito <muhammad.daffa23@ui.ac.id>
2023-02-27 12:11:48 +0700

    changes to generateId method

--- assignment1/src/main/java/assignments/assignment1/NotaGenerator.java
+++ assignment1/src/main/java/assignments/assignment1/NotaGenerator.java
@@ -1,15 +1,51 @@
 package assignments.assignment1;
 
+// Mengimport module
 import java.util.Scanner;
 
 public class NotaGenerator {
     private static final Scanner input = new Scanner(System.in);
-
-    /**
-     * Method main, program utama kalian berjalan disini.
-     */
+    public static boolean looping = true;
+    
+    // Method main, program utama kalian berjalan disini.
     public static void main(String[] args) {
         // TODO: Implement interface menu utama
+        while (looping) {
+            printMenu();
+            System.out.print("Pilihan: ");
+            String pilihan = input.nextLine();
+            System.out.println("================================");
+            
+            // input.nextLine(); // hilangkan nanti, untuk sekarang save saja dulu
+
+            switch (pilihan) {
+                case "1":
+                    System.out.print("Masukkan nama Anda: ");
+                    String nama = input.next();
+                    input.nextLine();
+                    System.out.print("Masukkan nomor handphone Anda: ");
+                    String nomorHP = input.nextLine();
+                    while (!nomorHP.matches("\\d+")) { // Checking if input is digit with regex
+                        System.out.println("Nomor hp hanya menerima digit");
+                        nomorHP = input.nextLine();
+                    }
+                    String id = generateId(nama, nomorHP);
+                    System.out.println("ID Anda : " + id);
+                    break;
+                case "2":
+                    // generateNota(null, null, pilihan, null);
+                    break;
+                case "0":
+                    System.out.println("Terima kasih telah menggunakan NotaGenerator!");
+                    looping = false;
+                    break;
+                default:
+                    System.out.println("Perintah tidak diketahui, silahkan periksa kembali.");
+                    
+
+            }
+            
+        }
     }
 
     /**
@@ -33,23 +69,38 @@ public class NotaGenerator {
         System.out.println("| Reguler | 3 Hari |  7000 / Kg |");
         System.out.println("+-------------------------------+");
     }
-
+    
     /**
      * Method untuk membuat ID dari nama dan nomor handphone.
-     * Parameter dan return type dari method ini tidak boleh diganti agar tidak mengganggu testing
-     *
      * @return String ID anggota dengan format [NAMADEPAN]-[nomorHP]-[2digitChecksum]
      */
     public static String generateId(String nama, String nomorHP){
-        // TODO: Implement generate ID sesuai soal.
-        return null;
+        String namaKapital = nama.toUpperCase();
+        String idBefore = namaKapital + "-" + nomorHP;
+        String idAfter = "";
+        int checksum = 0;
+        for (int i = 0; i < idBefore.length(); i++) {
+            char letter = idBefore.charAt(i);
+            if (Character.isUpperCase(letter)) {
+                int position = (letter - 'A') + 1;
+                checksum += position;
+            } else if (Character.isDigit(letter)) {
+                checksum += Character.getNumericValue(letter);
+            } else {
+                checksum += 7;
+            }
+        }
+        if (checksum < 10) {
+            idAfter = idBefore + "-0" + checksum;
+        } else {
+            idAfter = idBefore + "-" + checksum;
+        }
+        return idAfter;
     }
 
     /**
      *
      * Method untuk membuat Nota.
-     * Parameter dan return type dari method ini tidak boleh diganti agar tidak mengganggu testing.
-     *
      * @return string nota dengan format di bawah:
      *         <p>ID    : [id]
      *         <p>Paket : [paket]
